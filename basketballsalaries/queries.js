66876f15d1e6c05fadb32d95b437302eca943472
Cole Vahey <rcvahey01@gmail.com>
2017-10-17 21:00:52 -0400

    Changes

--- basketballsalaries/queries.js
+++ basketballsalaries/queries.js
@@ -1,34 +1,17 @@
-const pg = require('pg')
-const { graphql, buildSchema } = require('graphql');
+let pg = require('pg')
+let graphql = require('graphql');
 
-const pool = new pg.Pool()
+console.log("Your team is the " + teamname)
 
-pool.connect()
+var test = function() {
+  let pool = new pg.Pool()
 
-pool.query('SELECT * FROM salaries', (err, res) => {
-  if (err) {
-    throw err
-  }
+  pool.connect()
+  pool.query('SELECT * FROM salaries where team = \'' + teamname + '\'', (err, res) => {
+    if (err) {
+      throw err
+    }
 
-  console.log('user:', res.rows)
-  pool.end()
-})
-
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
-
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+    console.log('Chicago Bulls:', res.rows)
+  })
+}
--- basketballsalaries/teamplayers.sh
+++
@@ -1,1 +0,0 @@
-psql -U ColeV -d ColeV -c "SELECT *  FROM salaries WHERE TEAM = '$1'"
