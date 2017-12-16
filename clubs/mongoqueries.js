d3da1704f56e2d28f5adb139d3b95977f4f1a0f8
colevahey <rcvahey01@gmail.com>
2017-12-16 17:21:47 -0500

    Did a lot with the mongo queries

--- clubs/mongoqueries.js
+++ clubs/mongoqueries.js
@@ -1,7 +1,22 @@
 conn = new Mongo()
 db = conn.getDB("test")
 
-selectall = db.basicinfo.find();
+/*selectall = db.basicinfo.find();
 while ( selectall.hasNext() ) {
    printjson( selectall.next() );
 }
+
+selectsome = db.basicinfo.find({"FirstName" : "Sam"});
+while ( selectsome.hasNext() ) {
+   printjson( selectsome.next() );
+}
+
+//removeall = db.basicinfo.remove({});
+
+insert = db.basicinfo.insert({"test":"passed"})
+insertafew = db.basicinfo.insert([
+  {"newinsert":"here"},
+  {_id:20,"OnewithID":"yep"}
+])*/
+
+join = db.basicinfo.aggregate([{$lookup:{from:"haircolors", localField: "ColorID", foreignField: "Hair Color", as: "colormatches"}}])
